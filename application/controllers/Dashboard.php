ed23f36eaeb23dd2b129df3e23c62abb15297b50
Mitchell Jolivette <jolivetteme@gmail.com>
2018-08-01 14:59:53 -0500

    i don't kn0w where i am right now. Just read the code

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -19,8 +19,7 @@ class Dashboard extends CI_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 
-	public function __construct()
-	{
+	public function __construct(){
 		# code...
 		parent::__construct();
 		$this->load->model('dashboard_model');
@@ -29,14 +28,59 @@ class Dashboard extends CI_Controller {
 		$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 		$this->output->set_header('Pragma: no-cache');
 		$this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-		
+
 	}
-	public function index()
-	{
+	public function index(){
 		$data['pageTitle'] = 'Dashboard';
+
 		$this->load->view('dashboard/index',$data);
 	}
-	public function loadLocations() {}
-	public function loadTrucks() {}
+	/**
+	 * USED IN AJAX REQUEST TO SEE IF THE USER EXISTS ALREADY.
+	 * REMEMBER, ALWAYS ECHO RETURENED DATA FOR JSON
+	*/
+	public function isEmailRegistered() {
+		$email = $this->input->post('email');
 
+
+		$emailResults = $this->dashboard_model->db->query("SELECT id,email FROM users WHERE ((email LIKE ?)) AND deleted = ?",array($email,0))->num_rows();
+
+		// echo "Hello, World {$emailResults}";die;
+		if ($emailResults) {
+			echo json_encode(["error"=>"Email Registered! Please choose another name"]);
+		}
+
+
+	}
+	public function isUserRegistered($username) {
+
+		$users = $this->dashboard_model->db->query("SELECT id,username FROM users WHERE ((username LIKE ?)) AND deleted = ?",array($username,0))->num_rows();
+
+		if ($users) {
+			echo json_encode(["error"=>"User Exists! Please choose another name"]);
+		}
+	}
+	public function register() {
+
+
+		$username = $this->input->post('username');
+		$email 		= $this->input->post('email');
+
+
+		if ($this->isEmailRegistered($email)){
+			$messages = ['error'=>"Email Alreay Exists. Please choose another Email."];
+			echo json_encode($messages);
+
+		}
+		if ($this->isUserRegistered($username)){
+			$messages = ['error'=>"Username Alreay Exists. Please choose another username."];
+			echo json_encode($messages);
+
+		}
+		// return json_encode(true);
+		// if (isUserRegistered($username, $email){
+		// 	return json_encode("This user already exists");
+		// })
+		// return json_encode(array('mitchell','edwin','jolivette'));
+	}
 }
--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -4,32 +4,40 @@ class Dashboard_model extends CI_Model {
 
 	public $variable;
 
-	public function __construct()
-	{
+	public function __construct(){
 		parent::__construct();
 		$this->load->database();
-		
+
 	}
-	public function getLocations($current=NULL)
-	{
+	public function getLocations($current=NULL){
 		# code...
 		$query = $this->db->get('locations');
 		return $query->result_array();
 	}
-	public function addLocatons($businessName=null)
-	{
-		# code...
-		
-		$query = $this->db->query("SELECT businessName, lat, lng FROM locations");
-		$businessNames = $query->result_array();
-		foreach ($businessNames as $name) {
-			//INSERT INTO locatins
+	public function addLocatons($businessName=null){
+			# code...
+			$query = $this->db->query("SELECT businessName, lat, lng FROM locations");
+			$businessNames = $query->result_array();
+			foreach ($businessNames as $name) {
+				//INSERT INTO locatins
 
-			//BUSINESS NAME, LAT, LNG
-		}
+				//BUSINESS NAME, LAT, LNG
+			}
+	}
+	public function insertData($data) {
+		echo "Inserting data ...";
 
 	}
-
+	public function register() {
+		// $query = $this->db->query('SELECT username, email FROM users WHERE username LIKE ? AND email LIKE ?; ');
+		// $data = $query->result();
+		//
+		// foreach ($data as $key => $value) {
+		// 	// code...
+		// 	$users[] = $value->username;
+		// }
+		// return $users;
+	}
 }
 
 /* End of file Dashboard_model.php */
